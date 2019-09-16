defaaec7ab811e38c697ce33528e4b003389f1c7
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-09-16 08:37:16 -0700

    added db-config file. added get method in user-model. used get method in user-router

---
+++ api/models/user-model.js
@@ -0,0 +1,12 @@
+const db = require('../../data/db-config.js');
+
+
+module.exports = {
+    get
+}
+
+
+function get() {
+    return db('users')
+    .select()
+}
--- api/routes/user-router.js
+++ api/routes/user-router.js
@@ -1,9 +1,16 @@
 const express = require('express');
-
 const router = express.Router();
 
+const Users = require('../models/user-model.js');
+
 router.get('/', (req, res) => {
-    res.send('<h1>USERS GO HERE</h1>');
+    Users.get()
+    .then(users => {
+        res.status(200).json(users);
+    })
+    .catch(err => {
+        res.status(500).json({ error: 'Could not retrieve user data.' });
+    })
 })
 
 
