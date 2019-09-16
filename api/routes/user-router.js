a9674bca932c3af70052d2faf87ee9e84247c1ee
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-09-16 13:47:00 -0700

    completed MVP

--- api/routes/user-router.js
+++ api/routes/user-router.js
@@ -17,32 +17,6 @@ router.get('/', restricted, (req, res) => {
     })
 })
 
-// REGISTER USER
-router.post('/', validateUser, (req, res) => {
-    const {username, password} = req.body;
-    const hash = bcrypt.hashSync(password, 12);
-
-    Users.add({ username, password: hash })
-    .then(added => {
-        res.status(201).json(added);
-    })
-    .catch(err => {
-        res.status(500).json({ error: 'Server error' });
-    })
-})
-
-// MIDDLEWARE
-
-function validateUser(req, res, next) {
-    const { username, password } = req.body;
-
-    if (username && password) {
-        next();
-    } else {
-        res.status(400).json({ message: 'Must provide a username and password to register.' });
-    }
-}
-
 
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ const helmet = require('helmet');
 
 const userRouter = require('./routes/user-router.js');
 const loginRouter = require('./routes/login-router.js');
+const registerRouter = require('./routes/register-router.js');
 
 const server = express();
 
@@ -11,6 +12,7 @@ server.use(helmet());
 
 server.use('/api/users', userRouter);
 server.use('/api/login', loginRouter);
+server.use('/api/register', registerRouter);
 
 
 module.exports = server;
